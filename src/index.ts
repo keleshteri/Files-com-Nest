220c72eafb5f51349eada4dd288d78b7dcdcb611
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@outlook.com>
2024-08-08 13:31:23 +0800

    patch version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './files-com.module';
-export * from './services/files-com.service';
-export * from './interfaces';
-export * from './configs';
+import { FilesComModule } from './files-com.module';
+import { FilesComApiService, FilesComService } from './services';
+
+export { FilesComModule, FilesComService, FilesComApiService };
