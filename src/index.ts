47e535bce61a9292e82477a4c3ca7e4b40ddbe54
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@outlook.com>
2024-08-08 14:21:08 +0800

    feat: Add Files.com configuration to exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import { FilesComConfig } from './configs';
 import { FilesComModule } from './files-com.module';
 import { FileResponse } from './interfaces';
 import { FilesComApiService, FilesComService } from './services';
 
-export { FilesComModule, FileResponse, FilesComService, FilesComApiService };
+export {
+  FilesComModule,
+  FileResponse,
+  FilesComService,
+  FilesComApiService,
+  FilesComConfig,
+};
